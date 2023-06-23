140597d5e7376bc2e0c3de340079219b546b63c3
vitalik <vitalgreg1988@gmail.com>
2023-06-23 13:46:08 +0300

    b

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-import { disableReactDevTools } from 'fvilers/disable-react-devtools';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);

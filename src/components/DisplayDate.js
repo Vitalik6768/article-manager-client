a7d87d74051a9c7c80644fc9536b7ce627fd26f2
vitalik <vitalgreg1988@gmail.com>
2023-08-04 10:20:53 +0300

    improve responsive2

--- src/components/DisplayDate.js
+++ src/components/DisplayDate.js
@@ -5,7 +5,9 @@ import Typography from '@mui/material/Typography';
 function DisplayDate(props) {
     let month = props.date;
   return (
-    <Typography data-testid="todo-1" variant='h7'>מאמרים לחודש: {month} </Typography>
+    <Typography data-testid="todo-1" variant='h5' sx={{
+      color: "secondary"
+    }}> {month} </Typography>
    
     
   );
